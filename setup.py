9af14d8a42990f0cdd6535cb6c2b1abf922025e1
Thomas Konstantinovsky <56339082+MuteJester@users.noreply.github.com>
2025-09-20 13:14:41 +0300

    Ready for V0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='GenAIRR',
-    version='0.5.2',
+    version='0.6.0',
     author='Thomas Konstantinovsky & Ayelet Peres',
     author_email='thomaskon90@gmail.com',
     description='An advanced immunoglobulin sequence simulation suite for benchmarking alignment models and sequence analysis.',
